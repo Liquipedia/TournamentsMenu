2d18167520289cdadae28f937b4e1ddcfd6d654b
FO-nTTaX <fo.nttax@yahoo.de>
2016-10-03 16:09:38 +0200

    check if output has a title set

--- TournamentsMenuParserFunction.php
+++ TournamentsMenuParserFunction.php
@@ -124,7 +124,7 @@ class TournamentsMenuParserFunction {
 					$return .= '<li>';
 					$return .= '<a ' . ((!$tournament_arr['exists'])?'class="new" ':'') . 'href="' . $tournament_arr['href'] . '">';
 					$return .= '<span class="tournaments-list-name">';
-					if( isset( $tournament_arr['icon'] ) && ( Title::newFromText( $iconTemplatePrefix . '/' . $tournament_arr['icon'], NS_TEMPLATE )->exists() ) && ($wgOut->getTitle() != null) ) {
+					if( isset( $tournament_arr['icon'] ) && ( Title::newFromText( $iconTemplatePrefix . '/' . $tournament_arr['icon'], NS_TEMPLATE )->exists() ) && ( $wgOut->getTitle() != null ) ) {
 						$return .= str_replace( '<p>', '', str_replace( '</p>', '', $wgOut->parse( '{{' . $iconTemplatePrefix . '/' . $tournament_arr['icon'] . '|link=}}' ) ) );
 					}
 					$return .= $tournament_arr['text'] . '</span>';
