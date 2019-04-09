5407a27111d8552f7f63f84ea22a5a14958f717b
FO-nTTaX <fo.nttax@yahoo.de>
2019-04-09 17:37:13 +0200

    icons

--- TournamentsMenuParserFunction.php
+++ TournamentsMenuParserFunction.php
@@ -183,7 +183,7 @@ class TournamentsMenuParserFunction {
 						$iconTitle = null;
 					}
 					if ( isset( $tournament_arr[ 'icon' ] ) && !is_null( $iconTitle ) && $iconTitle->exists() && !is_null( $parser->getTitle() ) ) {
-						$iconHTML = $parser->parse( '{{' . $iconTemplatePrefix . '/' . $tournament_arr[ 'icon' ] . '|link=}}', $parser->getTitle(), $parser->getOptions(), false, false )->getText();
+						$iconHTML = $parser->getFreshParser()->parse( '{{' . $iconTemplatePrefix . '/' . $tournament_arr[ 'icon' ] . '|link=}}', $parser->getTitle(), $parser->getOptions(), false, false )->getText();
 						$from = '<span';
 						$to = '</span>';
 						if ( strpos( $iconHTML, $from ) !== false && strpos( $iconHTML, $to ) !== false ) {
